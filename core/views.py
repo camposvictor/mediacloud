799a72fae8297b69aa348589616dc8bed3a2043e
lhaquino22 <99754185+lhaquino22@users.noreply.github.com>
2024-08-09 23:14:04 -0300

    modificação de importação

--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import MediaFileForm
 from .models import MediaFile
+from django.db.models import Q
 
 @login_required
 def dashboard(request):
